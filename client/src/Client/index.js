e9ca08efee8321343fb2993ac4a669b4112772bb
hrugved <whrugved@gmail.com>
2022-04-22 01:50:03 +0530

    add register

--- client/src/Client/index.js
+++ client/src/Client/index.js
@@ -1,10 +1,37 @@
 import React, { Fragment, useEffect, useState } from "react";
 import styles from "./styles.module.css";
+import axios from "axios-server"
 
 const Client = (props) => {
 
+  const [ip,setIp] = useState("0.0.0.0")
+  const [system,setSystem] = useState("")
+
   const [init,setInit] = useState(false)
   const [name,setName] = useState("")
+  // const [name,setName] = useState("Hrugved")
+  const [active,setActive] = useState(false)
+  const [id,setId] = useState(-1)
+
+  const register = async (_ip,_system) => {
+    const { data } = await axios.post('/register', {
+      client_name: name,
+      client_system: _system,
+      ip: _ip
+    });
+    setId(data.data.client_id)
+  }
+
+  useEffect(() => {
+    if(init) {
+      const ip_ = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))
+      setIp(ip_)
+      const arr = ['Window','Linux','Mac']
+      const system_ = arr[Math.floor(Math.random() * arr.length)]
+      setSystem(system_)
+      register(ip_,system_)
+    }
+  },[init])
 
   return (
     <div className={styles.root}>
@@ -17,31 +44,40 @@ const Client = (props) => {
         </div>
       )}
       {init && (
+      // {!init && (
         <Fragment>
       <div className={styles.info}>
         <div className={styles.name_box}>
           <p className={styles.name}>{name}</p>
         </div>
+        <div className={styles.system_box}>
+          <p className={styles.system}>{system}</p>
+        </div>
         <div className={styles.ip_box}>
-          <p className={styles.ip}>192.168.192.136</p>
+          <p className={styles.ip}>{ip}</p>
         </div>
-        <div className={styles.inactive_box}>
+        {active && (<div className={styles.active_box}>
+          <p className={styles.status}>active</p>
+        </div>)}
+
+        {!active && (<div className={styles.inactive_box}>
           <p className={styles.status}>inactive</p>
-        </div>
+        </div>)}
+        
       </div>
       
       <div className={styles.timer_box}>
-        <p className={styles.timer}>00:00:00</p>
-        {/* <p className={styles.timer_stop}>00:00:00</p> */}
+        {active && <p className={styles.timer}>00:00:00</p>}
+        {!active && <p className={styles.timer_stop}>00:00:00</p>}
       </div>
 
       <div className={styles.lower_box}>
 
         <div className={styles.control_box}>
-          <div className={styles.start_box}>
+          <div className={styles.start_box} onClick={() => setActive(true)}>
             <p className={styles.start}>start</p>
           </div>
-          <div className={styles.stop_box}>
+          <div className={styles.stop_box} onClick={() => setActive(false)}>
             <p className={styles.stop}>stop</p>
           </div>
         </div>
