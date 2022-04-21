e78f0d0769a84ab5d4dd3b86efa5497ea666c135
hrugved <whrugved@gmail.com>
2022-04-21 05:30:49 +0530

    add log api

--- server/predict.js
+++ server/predict.js
@@ -16,26 +16,20 @@ const predict = (data) => {
   })
 }
 
+const format_data = (data) => {
+  // console.log(Object.entries(data));
+  data = Object.fromEntries(Object.entries(data).map(([k,v]) => [k,{'0':v}]))
+  // console.log(data);
+  return data
+}
+
 // const data = JSON.parse('{"pkSeqID":{"0":3142762},"proto":{"0":"udp"},"saddr":{"0":"192.168.100.150"},"sport":{"0":"6551"},"daddr":{"0":"192.168.100.3"},"dport":{"0":"80"},"seq":{"0":251984},"stddev":{"0":1.900363},"N_IN_Conn_P_SrcIP":{"0":100},"min":{"0":0.0},"state_number":{"0":4},"mean":{"0":2.687519},"N_IN_Conn_P_DstIP":{"0":100},"drate":{"0":0.0},"srate":{"0":0.494549},"max":{"0":4.031619}}')
+
 const data = {
-    pkSeqID: { '0': 3142762 },
-    proto: { '0': 'udp' },
-    saddr: { '0': '192.168.100.150' },
-    sport: { '0': '6551' },
-    daddr: { '0': '192.168.100.3' },
-    dport: { '0': '80' },
-    seq: { '0': 251984 },
-    stddev: { '0': 1.900363 },
-    N_IN_Conn_P_SrcIP: { '0': 100 },
-    min: { '0': 0 },
-    state_number: { '0': 4 },
-    mean: { '0': 2.687519 },
-    N_IN_Conn_P_DstIP: { '0': 100 },
-    drate: { '0': 0 },
-    srate: { '0': 0.494549 },
-    max: { '0': 4.031619 }
+    pkSeqID: "3142762",
+    proto: 'udp'
   }
-console.log(parse_result(data));
+console.log(format_data(data));
 const data_json = JSON.stringify(data)
 // console.log(data_json);
 predict(data_json)
--- server/router.js
+++ server/router.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const db = require('./connection')
+const {predict} = require('./utils')
 const router = new express.Router()
+
+const MAX = 733705
 
 // create task
 router.get('/ping', async (req,res ) => {
     res.status(200).send("pong")
 })
+
+router.get('/send', async (req,res) => {
+    const id = Math.floor(Math.random() * MAX) + 1;
+    const sql = `SELECT * FROM packet where id=${id}`;
+    db.query(sql, function (err, data) {
+        if (err) {
+            res.status(400).send(null);
+            throw err;
+        }
+        predict(data[0],req.query.client_id)
+        res.status(200).send(data);
+    });
+})
+
+
 
 // router.get('/semesters', async (req,res) => {
 //     const sql = `SELECT semester FROM course GROUP BY semester`;
